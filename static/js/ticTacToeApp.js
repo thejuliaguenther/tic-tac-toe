57a60d37ac5d47e0ee3455bb71d57f288d97c4fb
Julia Guenther <juliag1@alumni.stanford.edu>
2016-03-08 19:44:25 -0800

    gameController initialized

--- static/js/ticTacToeApp.js
+++ static/js/ticTacToeApp.js
@@ -5,7 +5,45 @@ ticTacToeApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         
     .when('/', {
-        templateUrl: 'static/pages/game.html'
-        })
+        templateUrl: 'static/pages/game.html',
+        controller:'gameController'
+        });
         
+}]);
+
+ticTacToeApp.value('gamePieceX', 'X');
+ticTacToeApp.value('gamePieceO', 'O');
+
+ticTacToeApp.factory('BoardService', function(){
+    var factory = {};
+
+    factory.createNewBoard = function(){
+        var board = [];
+        for(var i = 0; i < 9; i++){
+            board.push[null];
+        }
+        return board;
+    }
+
+});
+
+//Can be many instances of the same type of controller in a game 
+ticTacToeApp.controller('gameController',['$scope', 'gamePieceX', 'gamePieceO', 'BoardService', function($scope, gamePieceX, gamePieceO, BoardService){
+
+    $scope.X = gamePieceX;
+    $scope.O = gamePieceO;
+    // $scope.moveCount = 0;
+
+    $scope.board = BoardService.createNewBoard();
+
+    
+
+    // $scope.makeMove = function(moveCount){
+    //     if moveCount === 0{
+    //         return;
+        
+    //     }
+    // };
+
+
 }]);
